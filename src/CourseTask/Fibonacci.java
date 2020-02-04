cbdf4b251474bae4d6d80444906568cdade4f631
WorstDeveloper <my@email.com>
2020-02-04 20:51:19 +0700

    Курсовая задача последовательность Фироначчи

---
+++ src/CourseTask/Fibonacci.java
@@ -0,0 +1,35 @@
+package CourseTask;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number in the sequence:");
+        int searchNumber = scanner.nextInt();
+
+        int fibonacciN0 = 0;
+        int fibonacciN1 = 1;
+
+        if (searchNumber == 0){
+            System.out.println("Answer: "+ fibonacciN0);
+            return;
+        }
+        if (searchNumber == 1){
+            System.out.println("Answer: "+ fibonacciN1);
+            return;
+        }
+
+        int fibonacciN2 = 0;
+        int i = 3;
+        while (i<=searchNumber){
+            fibonacciN2 = fibonacciN0 + fibonacciN1;
+            fibonacciN0 = fibonacciN1;
+            fibonacciN1 = fibonacciN2;
+            i++;
+        }
+        System.out.println("Answer: "+ fibonacciN2);
+
+    }
+}
