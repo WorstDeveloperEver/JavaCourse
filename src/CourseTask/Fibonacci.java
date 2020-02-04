8364191351dd2a6924fbd0add56bfbb70fbb9029
WorstDeveloper <my@email.com>
2020-02-04 20:55:45 +0700

    Курсовая задача последовательность Фибоначчи

--- src/CourseTask/Fibonacci.java
+++ src/CourseTask/Fibonacci.java
@@ -9,27 +9,32 @@ public class Fibonacci {
         System.out.println("Enter the number in the sequence:");
         int searchNumber = scanner.nextInt();
 
+        if (searchNumber < 0) {
+            System.out.println("Wrong number");
+            return;
+        }
+
         int fibonacciN0 = 0;
         int fibonacciN1 = 1;
 
-        if (searchNumber == 0){
-            System.out.println("Answer: "+ fibonacciN0);
+        if (searchNumber == 0) {
+            System.out.println("Answer: " + fibonacciN0);
             return;
         }
-        if (searchNumber == 1){
-            System.out.println("Answer: "+ fibonacciN1);
+        if (searchNumber == 1) {
+            System.out.println("Answer: " + fibonacciN1);
             return;
         }
 
         int fibonacciN2 = 0;
         int i = 3;
-        while (i<=searchNumber){
+        while (i <= searchNumber) {
             fibonacciN2 = fibonacciN0 + fibonacciN1;
             fibonacciN0 = fibonacciN1;
             fibonacciN1 = fibonacciN2;
             i++;
         }
-        System.out.println("Answer: "+ fibonacciN2);
+        System.out.println("Answer: " + fibonacciN2);
 
     }
 }
