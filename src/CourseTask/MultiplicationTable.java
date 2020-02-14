a2f124b6e057a7d62e8f660b518f26f0c2bf33cb
WorstDeveloper <my@email.com>
2020-02-14 22:55:11 +0700

    Add space, a lot of space

--- src/CourseTask/MultiplicationTable.java
+++ src/CourseTask/MultiplicationTable.java
@@ -5,17 +5,20 @@ public class MultiplicationTable {
         final int tableSize = 10;
 
         System.out.printf("%n     ");
+
         for (int i = 0; i < tableSize; i++) {
             System.out.printf("%4d", i + 1);
         }
 
         System.out.printf("%n     ");
+
         for (int i = 0; i < tableSize; i++) {
             System.out.print("----");
         }
 
         for (int i = 0; i < tableSize; i++) {
             System.out.printf("%n%3d |", i + 1);
+
             for (int j = 0; j < tableSize; j++) {
                 System.out.printf("%4d", (i + 1) * (j + 1));
             }
