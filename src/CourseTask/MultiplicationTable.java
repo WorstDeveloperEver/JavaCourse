c7da42f3e8d3d593eeaa17f23df887747aa9f61f
WorstDeveloper <my@email.com>
2020-02-14 22:48:36 +0700

    Break one big cycles to 3 small

--- src/CourseTask/MultiplicationTable.java
+++ src/CourseTask/MultiplicationTable.java
@@ -3,25 +3,20 @@ package CourseTask;
 public class MultiplicationTable {
     public static void main(String[] args) {
         final int tableSize = 10;
-        int printedNumber;
 
-        for (int i = -1; i <= tableSize; i++) {
-            for (int j = -1; j <= tableSize; j++) {
-                printedNumber = Math.abs(i * j);
+        System.out.printf("%n     ");
+        for (int i = 0; i < tableSize; i++) {
+            System.out.printf("%4d", i + 1);
+        }
+        System.out.printf("%n     ");
+        for (int i = 0; i < tableSize; i++) {
+            System.out.print("----");
+        }
 
-                if (j == 0) {
-                    System.out.print(" | ");
-                } else if (i == 0) {
-                    System.out.print("----");
-                }
-
-                if (i != 0 && j != 0) {
-                    System.out.printf("%4d", printedNumber);
-                }
-
-                if (j == tableSize) {
-                    System.out.println();
-                }
+        for (int i = 0; i < tableSize; i++) {
+            System.out.printf("%n%3d |", i + 1);
+            for (int j = 0; j < tableSize; j++) {
+                System.out.printf("%4d", (i + 1) * (j + 1));
             }
         }
     }
