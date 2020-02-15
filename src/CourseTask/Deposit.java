331d530fbaf70f5208c1c5e5da45c2329aa162fa
WorstDeveloper <my@email.com>
2020-02-15 10:04:02 +0700

    Moved variable creation to loop

--- src/CourseTask/Deposit.java
+++ src/CourseTask/Deposit.java
@@ -4,13 +4,12 @@ public class Deposit {
     public static void main(String[] args) {
         int depositPeriodInMonths = 12;
         double depositAmount = 100000;
-        double depositProfitInMonth;
         double percent = 0.06;
         double depositAfterPeriod = depositAmount;
 
         for (int i = 0; i < depositPeriodInMonths; i++) {
-            depositProfitInMonth = depositAfterPeriod * percent / depositPeriodInMonths;
-            depositAfterPeriod = depositAfterPeriod + depositProfitInMonth;
+            double depositProfitInMonth = depositAfterPeriod * percent / depositPeriodInMonths;
+            depositAfterPeriod += depositProfitInMonth;
         }
 
         System.out.printf("Deposit amount after period = %.2f, profit = %.2f", depositAfterPeriod, depositAfterPeriod - depositAmount);
