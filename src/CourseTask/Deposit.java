5f5f0434f50e043b73c686678b3d0c8b5bea7458
WorstDeveloper <my@email.com>
2020-02-19 01:16:26 +0700

    Fix divider on loop, need divide on 12 month

--- src/CourseTask/Deposit.java
+++ src/CourseTask/Deposit.java
@@ -1,17 +1,28 @@
 package CourseTask;
 
+import java.util.Scanner;
+
 public class Deposit {
     public static void main(String[] args) {
-        int depositPeriodInMonths = 12;
-        double depositAmount = 100000;
-        double percent = 0.06;
+        double depositPeriodInMonths = getUserNumber("deposit period in months");
+        double depositAmount = getUserNumber("deposit amount");
+        double percent = getUserNumber("percent") / 100;
         double depositAfterPeriod = depositAmount;
 
         for (int i = 0; i < depositPeriodInMonths; i++) {
-            double depositProfitInMonth = depositAfterPeriod * percent / depositPeriodInMonths;
+            double depositProfitInMonth = depositAfterPeriod * percent / 12;
             depositAfterPeriod += depositProfitInMonth;
         }
 
-        System.out.printf("Deposit amount after period = %.2f, profit = %.2f", depositAfterPeriod, depositAfterPeriod - depositAmount);
+        double depositProfit = depositAfterPeriod - depositAmount;
+
+        System.out.printf("Deposit amount after period = %.2f, profit = %.2f", depositAfterPeriod, depositProfit);
+    }
+
+    public static double getUserNumber (String message){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Enter %s: ", message);
+        return scanner.nextDouble();
     }
 }
