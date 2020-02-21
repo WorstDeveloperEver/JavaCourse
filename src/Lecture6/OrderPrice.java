1b9aa74e2d9d5d0c42a51de98c38a025de5da41a
WorstDeveloper <my@email.com>
2020-02-21 23:39:37 +0700

    Rename variables discount

--- src/Lecture6/OrderPrice.java
+++ src/Lecture6/OrderPrice.java
@@ -10,24 +10,23 @@ public class OrderPrice {
         double goods1Price = 150;
         double goods2Price = 90;
 
-        double finalPrice = getFinalPrice(goods1Count, goods2Count, goods1Price, goods2Price);
-
+        double finalPrice = getFinalPrice(goods1Count, goods1Price, goods2Count, goods2Price);
         System.out.printf("Final price = %.2f rubles", finalPrice);
     }
 
-    public static double getFinalPrice(int goods1Count, int goods2Count, double goods1Price, double goods2Price) {
+    public static double getFinalPrice(int goods1Count, double goods1Price, int goods2Count, double goods2Price) {
         double goodsInOrderPrice = goods1Price * goods1Count + goods2Price * goods2Count;
 
         int discountValue = 5;
-        int discountByCount = 10;
-        int discountByPrice = 1000;
+        int goodsForDiscount = 10;
+        int priceForDiscount = 1000;
 
-        int discountCount = ((goods1Count + goods2Count) >= discountByCount) ? discountValue : 0;
-        double discountPrice = (goodsInOrderPrice >= discountByPrice) ? discountValue : 0;
+        int discountFromCount = ((goods1Count + goods2Count) >= goodsForDiscount) ? discountValue : 0;
+        double discountFromPrice = (goodsInOrderPrice >= priceForDiscount) ? discountValue : 0;
 
-        double discountFinal = (discountCount + discountPrice) / 100;
+        double finalDiscount = (discountFromCount + discountFromPrice) / 100;
 
-        return goodsInOrderPrice - goodsInOrderPrice * discountFinal;
+        return goodsInOrderPrice - goodsInOrderPrice * finalDiscount;
     }
 
     public static int getGoodsCount(String message) {
