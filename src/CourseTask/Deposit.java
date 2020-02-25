da02aeeabff2e54628e3e1503faa12bc1459b6e2
WorstDeveloper <my@email.com>
2020-02-25 23:34:50 +0700

    Rename variables...

--- src/CourseTask/Deposit.java
+++ src/CourseTask/Deposit.java
@@ -4,34 +4,28 @@ import java.util.Scanner;
 
 public class Deposit {
     public static void main(String[] args) {
-        int depositPeriodInMonths = getDepositPeriodInMonths("deposit period in months");
-        double depositAmount = getUserNumber("deposit amount");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter deposit period in months:");
+        int depositPeriodInMonths = scanner.nextInt();
+
+        System.out.print("Enter deposit amount:");
+        double depositAmount = scanner.nextDouble();
+
+        System.out.print("Enter percent:");
+        double percent = scanner.nextDouble();
 
         final int oneHundred = 100;
-        final int monthInYearCount = 12;
-        double percent = getUserNumber("percent") / (oneHundred * monthInYearCount);
+        final int monthsInYearCount = 12;
+        double percentPerMonth = percent / (oneHundred * monthsInYearCount);
 
         double depositAfterPeriod = depositAmount;
 
         for (int i = 0; i < depositPeriodInMonths; i++) {
-            depositAfterPeriod += depositAfterPeriod * percent;
+            depositAfterPeriod += depositAfterPeriod * percentPerMonth;
         }
 
         double depositProfit = depositAfterPeriod - depositAmount;
         System.out.printf("Deposit amount after period = %.2f, profit = %.2f", depositAfterPeriod, depositProfit);
-    }
-
-    public static double getUserNumber(String message) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.printf("Enter %s: ", message);
-        return scanner.nextDouble();
-    }
-
-    public static int getDepositPeriodInMonths(String message) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.printf("Enter %s: ", message);
-        return scanner.nextInt();
     }
 }
