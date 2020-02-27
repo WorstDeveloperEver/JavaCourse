f8a1137540723331ec51679914bbc89d207b2851
WorstDeveloper <my@email.com>
2020-02-27 15:31:54 +0700

    Add MultiplicationTableArray task

---
+++ src/Lecture9/MultiplicationTableArray.java
@@ -0,0 +1,28 @@
+package Lecture9;
+
+public class MultiplicationTableArray {
+    public static void main(String[] args) {
+        int size = 10;
+        int[][] multiplicationTable = getMultiplicationTable(size);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.printf("%4d", multiplicationTable[i][j]);
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static int[][] getMultiplicationTable(int size) {
+        int[][] multiplicationTable = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                multiplicationTable[i][j] = (j + 1) * (i + 1);
+            }
+        }
+
+        return multiplicationTable;
+    }
+}
