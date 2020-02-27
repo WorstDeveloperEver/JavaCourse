d0fc8b79f92686b1314b369aaf8245563eeb6d6e
WorstDeveloper <my@email.com>
2020-02-27 08:27:07 +0700

    Add single character string check

---
+++ src/CourseTask/MaxSubstring.java
@@ -0,0 +1,43 @@
+package CourseTask;
+
+import java.util.Scanner;
+
+public class MaxSubstring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter string:");
+        String userString = scanner.nextLine();
+
+        if (userString.length() == 1){
+            System.out.print("Max substring length = 1");
+            return;
+        }
+
+            System.out.printf("Max substring length = %d", getMaxSubstringLength(userString));
+    }
+
+    private static int getMaxSubstringLength(String userString) {
+        userString = userString.toLowerCase();
+
+        int maxSubstringLength = 0;
+        int equalCharsCount = 1;
+
+        for (int i = 0; i < userString.length() - 1; i++) {
+            char currentChar = userString.charAt(i);
+            char nextChar = userString.charAt(i + 1);
+
+            if (currentChar == nextChar) {
+                equalCharsCount++;
+            } else {
+                equalCharsCount = 1;
+            }
+
+            if (equalCharsCount > maxSubstringLength) {
+                maxSubstringLength = equalCharsCount;
+            }
+        }
+
+        return maxSubstringLength;
+    }
+}
