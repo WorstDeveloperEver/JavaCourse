80890b13c027261e31fefba9fa62f9eeab84d326
WorstDeveloper <my@email.com>
2020-03-03 14:57:46 +0700

    add EuclideanAlgorithmRecursion

---
+++ src/Lecture10/EuclideanAlgorithmRecursion.java
@@ -0,0 +1,32 @@
+package Lecture10;
+
+import java.util.Scanner;
+
+public class EuclideanAlgorithmRecursion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a:");
+        int a = scanner.nextInt();
+
+        System.out.println("Enter b:");
+        int b = scanner.nextInt();
+
+        if (a == 0 && b == 0) {
+            System.out.println("Wrong input");
+            return;
+        }
+
+        System.out.println("Largest common divider: " + getNOD(a, b));
+    }
+
+    public static int getNOD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        int remainder = a % b;
+
+        return getNOD(b, remainder);
+    }
+}
