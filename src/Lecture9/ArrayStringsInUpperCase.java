b67e7057a93a42a68e2d4ca52533124a597af878
WorstDeveloper <my@email.com>
2020-03-03 13:53:28 +0700

    Rename class Delete epsilon

--- src/Lecture9/ArrayStringsInUpperCase.java
+++ src/Lecture9/ArrayStringsInUpperCase.java
@@ -1,17 +1,17 @@
 package Lecture9;
 
-public class ArrayOfStringsInUpperCase {
+public class ArrayStringsInUpperCase {
     public static void main(String[] args) {
         String[] array = {"London ", "is ", "the ", "capital ", "of ", "Great ", "Britain"};
 
-        getUpperCaseString(array);
+        toUpperCaseString(array);
 
         for (String value : array) {
             System.out.print(value);
         }
     }
 
-    public static void getUpperCaseString(String[] array) {
+    public static void toUpperCaseString(String[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = array[i].toUpperCase();
         }
