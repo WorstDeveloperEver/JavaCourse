3651a7f6b4478d5f93e36a3e17778ed75b4f6b44
WorstDeveloper <my@email.com>
2020-03-03 13:53:15 +0700

    Rename class Delete epsilon

--- src/Lecture9/ArrayNumberMax.java
+++ src/Lecture9/ArrayNumberMax.java
@@ -1,22 +1,21 @@
 package Lecture9;
 
-public class GetMax {
+public class ArrayNumberMax {
     public static void main(String[] args) {
         double[] array = {2, 95, 15, 16, 89, 4, 9};
 
-        System.out.println(getMax(array));
+        System.out.println("Max number = " + getMax(array));
     }
 
     public static double getMax(double[] array) {
-        final double epsilon = 1e-10;
-        double max = array[0];
+        double maxNumber = array[0];
 
         for (double value : array) {
-            if (value - max > epsilon) {
-                max = value;
+            if (value > maxNumber) {
+                maxNumber = value;
             }
         }
 
-        return max;
+        return maxNumber;
     }
 }
