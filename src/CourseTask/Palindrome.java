332a9fa5410cf1c3dbba8f07674d879e6d42ad43
WorstDeveloper <my@email.com>
2020-03-03 13:32:11 +0700

    Delete isNoLettersString function Delete compareChar variable

--- src/CourseTask/Palindrome.java
+++ src/CourseTask/Palindrome.java
@@ -9,11 +9,6 @@ public class Palindrome {
         System.out.println("Enter string:");
         String userString = scanner.nextLine();
 
-        if (isNoLettersString(userString)) {
-            System.out.println("Wrong string, there is no letters");
-            return;
-        }
-
         System.out.printf("Is palindrome? %b", isPalindrome(userString));
     }
 
@@ -21,41 +16,22 @@ public class Palindrome {
         string = string.toLowerCase();
 
         for (int i = 0, j = string.length() - 1; i < string.length() / 2; i++, j--) {
-            char compareChar = string.charAt(i);
-
-            while (!Character.isLetter(compareChar)) {
-                compareChar = string.charAt(i + 1);
+            while (!Character.isLetter(string.charAt(i))) {
                 i++;
+                if (i == j) {
+                    return false;
+                }
             }
 
-            char leftChar = compareChar;
-
-            compareChar = string.charAt(j);
-
-            while (!Character.isLetter(compareChar)) {
-                compareChar = string.charAt(j - 1);
+            while (!Character.isLetter(string.charAt(j))) {
                 j--;
             }
-
-            char rightChar = compareChar;
 
             if (i == j) {
                 return true;
             }
 
-            if (leftChar != rightChar) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static boolean isNoLettersString(String string) {
-        char[] array = string.toCharArray();
-
-        for (char value : array) {
-            if (Character.isLetter(value)) {
+            if (string.charAt(i) != string.charAt(j)) {
                 return false;
             }
         }
