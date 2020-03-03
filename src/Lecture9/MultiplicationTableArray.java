90640ade79afd5a9e60d119ba91992902ddc72e7
WorstDeveloper <my@email.com>
2020-03-03 14:28:22 +0700

    Rename replace in condition size to length

--- src/Lecture9/MultiplicationTableArray.java
+++ src/Lecture9/MultiplicationTableArray.java
@@ -5,9 +5,9 @@ public class MultiplicationTableArray {
         final int size = 10;
         int[][] multiplicationTable = getMultiplicationTable(size);
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                System.out.printf("%4d", multiplicationTable[i][j]);
+        for (int[] ints : multiplicationTable) {
+            for (int i = 0; i < multiplicationTable.length; i++) {
+                System.out.printf("%4d", ints[i]);
             }
 
             System.out.println();
@@ -17,8 +17,8 @@ public class MultiplicationTableArray {
     public static int[][] getMultiplicationTable(int size) {
         int[][] multiplicationTable = new int[size][size];
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < multiplicationTable.length; i++) {
+            for (int j = 0; j < multiplicationTable.length; j++) {
                 multiplicationTable[i][j] = (j + 1) * (i + 1);
             }
         }
