2dc3c17a46ba7b7720e20f43872f2cd45fb418f1
WorstDeveloper <my@email.com>
2020-03-09 17:36:42 +0700

    Format

--- src/CourseTask/SelectionSort.java
+++ src/CourseTask/SelectionSort.java
@@ -8,24 +8,24 @@ public class SelectionSort {
 
         System.out.println("Sours array : " + Arrays.toString(array));
 
-        toSelectionSort(array);
+        doSelectionSort(array);
 
         System.out.println("Sorted array : " + Arrays.toString(array));
     }
 
-    public static void toSelectionSort(int[] array) {
-        for (int start = 0; start < array.length - 1; start++) {
-            int min = start;
+    public static void doSelectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
 
-            for (int i = start + 1; i < array.length; i++) {
-                if (array[i] < array[min]) {
-                    min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
                 }
             }
 
-            int temp = array[min];
-            array[min] = array[start];
-            array[start] = temp;
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
         }
     }
 }
