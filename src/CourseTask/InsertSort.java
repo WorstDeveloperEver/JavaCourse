e46b561a41661ae841e36753ef8a42818d87c1a1
WorstDeveloper <my@email.com>
2020-03-09 18:53:20 +0700

    Change output string rename function

--- src/CourseTask/InsertSort.java
+++ src/CourseTask/InsertSort.java
@@ -6,15 +6,16 @@ public class InsertSort {
     public static void main(String[] args) {
         int[] array = {2, 15, 1, 4, 89, 5, 8, 11};
 
-        System.out.println("Sours array : " + Arrays.toString(array));
+        System.out.println("User array: " + Arrays.toString(array));
 
-        toInsertSort(array);
+        doInsertSort(array);
 
-        System.out.println("Sorted array : " + Arrays.toString(array));
+        System.out.println("Sorted array: " + Arrays.toString(array));
     }
 
-    public static void toInsertSort(int[] array) {
+    public static void doInsertSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
+
             int temp = array[i];
             int j = i - 1;
 
@@ -26,7 +27,6 @@ public class InsertSort {
                 array[j + 1] = array[j];
                 j--;
             }
-
             array[j + 1] = temp;
         }
     }
