f57a130724fefdb3bb8b682511f2e4da04d8951a
WorstDeveloper <my@email.com>
2020-03-09 10:41:50 +0700

    Rename: searchIndex - index getBinarySearchRecursionIndex - binarySearchRecursion

--- src/CourseTask/BinarySearch.java
+++ src/CourseTask/BinarySearch.java
@@ -2,45 +2,45 @@ package CourseTask;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] a = {4, 8, 15, 16, 23, 42};
+        int[] array = {4, 8, 15, 16, 23, 42};
 
-        int searchIndex = getBinarySearchRecursionIndex(a, 0, a.length - 1, 42);
-        System.out.println("Searching by recursion, element index: " + searchIndex);
+        int index = binarySearchRecursion(array, 0, array.length - 1, 42);
+        System.out.println("Searching by recursion, element index: " + index);
 
-        searchIndex = getBinarySearchIndex(a, 42);
-        System.out.println("Searching without recursion, element index: " + searchIndex);
+        index = binarySearch(array, 42);
+        System.out.println("Searching without recursion, element index: " + index);
     }
 
-    public static int getBinarySearchRecursionIndex(int[] a, int left, int right, int x) {
+    public static int binarySearchRecursion(int[] array, int left, int right, int x) {
         if (left > right) {
             return -1;
         }
 
         int middle = (right + left) / 2;
 
-        if (a[middle] == x) {
+        if (array[middle] == x) {
             return middle;
         }
 
-        if (x > a[middle]) {
-            return getBinarySearchRecursionIndex(a, middle + 1, right, x);
+        if (x > array[middle]) {
+            return binarySearchRecursion(array, middle + 1, right, x);
         } else {
-            return getBinarySearchRecursionIndex(a, left, middle - 1, x);
+            return binarySearchRecursion(array, left, middle - 1, x);
         }
     }
 
-    public static int getBinarySearchIndex(int[] a, int x) {
+    public static int binarySearch(int[] array, int x) {
         int left = 0;
-        int right = a.length - 1;
+        int right = array.length - 1;
 
         while (left <= right) {
             int middle = (right + left) / 2;
 
-            if (a[middle] == x) {
+            if (array[middle] == x) {
                 return middle;
             }
 
-            if (x > a[middle]) {
+            if (x > array[middle]) {
                 left = middle + 1;
             } else {
                 right = middle - 1;
