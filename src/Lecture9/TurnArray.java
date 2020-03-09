83faf71944f1129a9391607dcc32f28be7a07067
WorstDeveloper <my@email.com>
2020-03-09 11:26:22 +0700

    Rename function

--- src/Lecture9/TurnArray.java
+++ src/Lecture9/TurnArray.java
@@ -6,14 +6,15 @@ public class TurnArray {
     public static void main(String[] args) {
         int[] array = {2, 95, 15, 16, 90, 6, 9, 10};
 
-        toTurnArray(array);
+        doTurnArray(array);
 
         System.out.print(Arrays.toString(array));
     }
 
-    public static void toTurnArray(int[] array) {
-        int arrayMiddle = array.length / 2;
-        for (int i = 0; i < arrayMiddle; i++) {
+    public static void doTurnArray(int[] array) {
+        int arrayMiddleIndex = array.length / 2;
+
+        for (int i = 0; i < arrayMiddleIndex; i++) {
             int temp = array[i];
             array[i] = array[array.length - (i + 1)];
             array[array.length - (i + 1)] = temp;
