cc9aed1177139496c1b1ee36b46d2e7ed392b42c
WorstDeveloper <my@email.com>
2020-03-10 19:58:11 +0700

    Accept

--- src/CourseTask/SelectionSort.java
+++ src/CourseTask/SelectionSort.java
@@ -6,14 +6,14 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] array = {2, 15, 1, 4, 89, 5, 8, 11};
 
-        System.out.println("Sours array : " + Arrays.toString(array));
+        System.out.println("Source array: " + Arrays.toString(array));
 
-        doSelectionSort(array);
+        SortBySelection(array);
 
-        System.out.println("Sorted array : " + Arrays.toString(array));
+        System.out.println("Sorted array: " + Arrays.toString(array));
     }
 
-    public static void doSelectionSort(int[] array) {
+    public static void SortBySelection(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             int minIndex = i;
 
