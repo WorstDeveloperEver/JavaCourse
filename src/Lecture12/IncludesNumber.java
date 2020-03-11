f2075e5285ba93fa8a9e039c1dbaf40f5da347d3
WorstDeveloper <my@email.com>
2020-03-11 12:42:37 +0700

    add function getContainsCount

--- src/Lecture12/IncludesNumber.java
+++ src/Lecture12/IncludesNumber.java
@@ -10,38 +10,42 @@ public class IncludesNumber {
 
         try (Scanner scanner = new Scanner(new FileInputStream("./src/Files/text.txt"))) {
             while (scanner.hasNextLine()) {
-                String string = scanner.nextLine();
-
-                int i = 0;
-
-                while (true) {
-                    int left = i;
-                    int right = string.indexOf(" ", i);
-
-                    if (right < left) {
-                        String compareString = string.substring(left).toLowerCase();
-
-                        if (compareString.contains("java")) {
-                            count++;
-                        }
-                        break;
-                    }
-
-                    String compareString = string.substring(left, right).toLowerCase();
-
-                    if (compareString.contains("java")) {
-                        count++;
-                    }
-
-                    if (right == string.length()) {
-                        break;
-                    }
-
-                    i = right + 1;
-                }
+                count+= getContainsCount(scanner.nextLine(), "java");
             }
         }
 
         System.out.println("The number of includes of the Java string in the file = " + count);
+    }
+
+    public static int getContainsCount(String string, String searchString){
+        int count = 0;
+        int i = 0;
+
+        while (true) {
+            int left = i;
+            int right = string.indexOf(" ", i);
+
+            if (right < left) {
+                String compareString = string.substring(left).toLowerCase();
+
+                if (compareString.contains(searchString)) {
+                    count++;
+                }
+                break;
+            }
+
+            String compareString = string.substring(left, right).toLowerCase();
+
+            if (compareString.contains(searchString)) {
+                count++;
+            }
+
+            if (right == string.length()) {
+                break;
+            }
+
+            i = right + 1;
+        }
+        return count;
     }
 }
