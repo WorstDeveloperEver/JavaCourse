c068c57b1a9e53edb5a93fc4444ab3eadd01d75c
WorstDeveloper <my@email.com>
2020-03-11 12:58:20 +0700

    rename class

--- src/Lecture12/ContainsNumber.java
+++ src/Lecture12/ContainsNumber.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class IncludesNumber {
+public class ContainsNumber {
     public static void main(String[] args) throws FileNotFoundException {
         int count = 0;
 
