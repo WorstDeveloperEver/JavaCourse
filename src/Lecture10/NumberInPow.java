7171e4610324f3e1e2998687af0046971818dbba
WorstDeveloper <my@email.com>
2020-03-13 16:18:14 +0700

    Rename function

--- src/Lecture10/NumberInPow.java
+++ src/Lecture10/NumberInPow.java
@@ -2,26 +2,28 @@ package Lecture10;
 
 public class NumberInPow {
     public static void main(String[] args) {
-        int degree = 6;
+        int power = 6;
         int number = 5;
 
-        System.out.printf("Number %d in degree %d = %d%n", number, degree, getNumberInPowRecursion(number, degree));
-        System.out.printf("Number %d in degree %d = %d", number, degree, getNumberInPow(number, degree));
+        System.out.printf("Number %d in power %d = %d%n", number, power, powRecursion(number, power));
+        System.out.printf("Number %d in power %d = %d", number, power, pow(number, power));
     }
 
-    public static int getNumberInPowRecursion(int number, int degree) {
-        if (degree == 0) {
+    public static int powRecursion(int number, int power) {
+        if (power == 0) {
             return 1;
         }
 
-        return number * getNumberInPowRecursion(number, degree - 1);
+        return number * powRecursion(number, power - 1);
     }
 
-    public static int getNumberInPow(int number, int degree) {
-        int sum = 1;
-        for (int i = 0; i < degree; i++) {
-            sum *= number;
+    public static int pow(int number, int power) {
+        int numberInPow = 1;
+
+        for (int i = 0; i < power; i++) {
+            numberInPow *= number;
         }
-        return sum;
+
+        return numberInPow;
     }
 }
