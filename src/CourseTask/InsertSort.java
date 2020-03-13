142d89eeb3e409df4cb71f8c0b54320e1303bec9
WorstDeveloper <my@email.com>
2020-03-13 16:22:45 +0700

    Rename function Now loop start from 1

--- src/CourseTask/InsertSort.java
+++ src/CourseTask/InsertSort.java
@@ -6,15 +6,15 @@ public class InsertSort {
     public static void main(String[] args) {
         int[] array = {2, 15, 1, 4, 89, 5, 8, 11};
 
-        System.out.println("User array: " + Arrays.toString(array));
+        System.out.println("Source array: " + Arrays.toString(array));
 
-        doInsertSort(array);
+        sort(array);
 
         System.out.println("Sorted array: " + Arrays.toString(array));
     }
 
-    public static void doInsertSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+    public static void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
             int temp = array[i];
             int j = i - 1;
 
@@ -26,6 +26,7 @@ public class InsertSort {
                 array[j + 1] = array[j];
                 j--;
             }
+
             array[j + 1] = temp;
         }
     }
