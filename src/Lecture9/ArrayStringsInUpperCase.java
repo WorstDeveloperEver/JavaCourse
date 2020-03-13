a8735a627d54a4a9e283fadbc597d39955d5f361
WorstDeveloper <my@email.com>
2020-03-13 16:35:28 +0700

    Rename function

--- src/Lecture9/ArrayStringsInUpperCase.java
+++ src/Lecture9/ArrayStringsInUpperCase.java
@@ -4,14 +4,14 @@ public class ArrayStringsInUpperCase {
     public static void main(String[] args) {
         String[] array = {"London ", "is ", "the ", "capital ", "of ", "Great ", "Britain"};
 
-        convertStringsToUppercase(array);
+        convertStringsToUpperCase(array);
 
         for (String value : array) {
             System.out.print(value);
         }
     }
 
-    public static void convertStringsToUppercase(String[] array) {
+    public static void convertStringsToUpperCase(String[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = array[i].toUpperCase();
         }
