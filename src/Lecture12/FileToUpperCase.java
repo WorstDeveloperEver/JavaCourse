779b4920516bf37f57e7da2873220320c7f1d0b0
WorstDeveloper <my@email.com>
2020-03-20 18:17:22 +0700

    Switch scanner on first place

--- src/Lecture12/FileToUpperCase.java
+++ src/Lecture12/FileToUpperCase.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 
 public class FileToUpperCase {
     public static void main(String[] args) throws FileNotFoundException {
-        try (PrintWriter writer = new PrintWriter("./src/Files/Result.txt");
-             Scanner scanner = new Scanner(new FileInputStream("./src/Files/text.txt"))) {
+        try (Scanner scanner = new Scanner(new FileInputStream("./src/Files/text.txt"));
+             PrintWriter writer = new PrintWriter("./src/Files/Result.txt")) {
             while (scanner.hasNextLine()) {
                 writer.println(scanner.nextLine().toUpperCase());
             }
